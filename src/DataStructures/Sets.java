60f21d1cfb5c7599f734579f2fd19c427e1468bb
Jaret Wright <hkkA223>
2020-10-23 12:16:49 -0400

    Added HashSet and TreeSet

--- src/DataStructures/Sets.java
+++ src/DataStructures/Sets.java
@@ -1,7 +1,43 @@
 package DataStructures;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class Sets {
     public static void main(String[] args) {
+        Student student1 = new Student("Fred","Flintstone",1001);
+        Student student2 = new Student("Barney","Rubble",1002);
+        Student student3 = new Student("Mr.", "Slate", 723);
+
+        ArrayList<Student> arrayList = new ArrayList<>();
+        arrayList.add(student1);
+        arrayList.add(student2);
+        arrayList.add(student3);
+        arrayList.add(student1);
+
+        for (Student student: arrayList)
+            student.increaseGrade();
+
+        //HashSet will not allow duplicates, you cannot predict the order of the
+        //elements in the Set
+        HashSet<Student> hashSet = new HashSet<>();
+        hashSet.add(student1);
+        hashSet.add(student2);
+        hashSet.add(student3);
+        hashSet.add(student1);
+
+        System.out.println("hashset size: "+hashSet.size());
+        System.out.println(hashSet);
+
+        //TreeSet - this is a set, so no duplicates, but adds the elements in order
+        TreeSet<Student> treeSet = new TreeSet<>();
+        treeSet.add(student1);
+        treeSet.add(student2);
+        treeSet.add(student3);
+        treeSet.add(student1);
+
 
     }
 }
--- src/DataStructures/Student.java
+++ src/DataStructures/Student.java
@@ -1,15 +1,22 @@
 package DataStructures;
 
-public class Student {
+public class Student implements Comparable<Student>{
     private String firstName, lastName;
     private int studentNum;
+    private int grade;
 
     public Student(String firstName, String lastName, int studentNum) {
         setFirstName(firstName);
         setLastName(lastName);
         setStudentNum(studentNum);
+        grade = 70;
     }
 
+    public void increaseGrade()
+    {
+        grade += 5;
+        if (grade > 100) grade = 100;
+    }
     public String getFirstName() {
         return firstName;
     }
@@ -35,5 +42,16 @@ public class Student {
             this.studentNum = studentNum;
         else
             throw new IllegalArgumentException("student number must be greater than 0");
+    }
+
+    public String toString()
+    {
+        return String.format("%d %s %s", studentNum, firstName, lastName);
+    }
+
+
+    @Override
+    public int compareTo(Student otherStudent) {
+        return this.studentNum-otherStudent.studentNum;
     }
 }
